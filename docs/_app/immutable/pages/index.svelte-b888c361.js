2085566d6440e6e207f76c79bc4bdb3ddf105717
austinypelara <apoberezhnik23@gmail.com>
2022-07-12 11:25:10 -0700

    Build static site

---
+++ docs/_app/immutable/pages/index.svelte-b888c361.js
@@ -0,0 +1,117 @@
+import{S as V,i as I,s as L,e as p,t as U,k as z,c as E,a as b,h as S,d as g,m as C,b as s,g as H,J as _,n as y,K as N,L as M,x as A,y as q,f as K,M as Q,z as j,N as R,r as x,p as T,C as D,O as ne,P as ie,Q as W,o as re,q as le,R as oe,T as Y,U as F,V as se}from"../chunks/index-faca8bb7.js";function ae(l){let e,n,t,i,a,h;return{c(){e=p("section"),n=p("div"),t=U("Challenge"),i=z(),a=p("div"),h=p("textarea"),this.h()},l(f){e=E(f,"SECTION",{class:!0});var r=b(e);n=E(r,"DIV",{class:!0});var o=b(n);t=S(o,"Challenge"),o.forEach(g),i=C(r),a=E(r,"DIV",{class:!0});var v=b(a);h=E(v,"TEXTAREA",{placeholder:!0,oninput:!0}),b(h).forEach(g),v.forEach(g),r.forEach(g),this.h()},h(){s(n,"class","challengeLeft svelte-znwfg0"),s(h,"placeholder","What's the Challenge?"),s(h,"oninput",'this.style.height = "";this.style.height = this.scrollHeight + "px"'),s(a,"class","challengeRight svelte-znwfg0"),s(e,"class","svelte-znwfg0")},m(f,r){H(f,e,r),_(e,n),_(n,t),_(e,i),_(e,a),_(a,h)},p:y,i:y,o:y,d(f){f&&g(e)}}}class he extends V{constructor(e){super(),I(this,e,null,ae,L,{})}}function ce(l){let e,n;return{c(){e=p("div"),n=p("textarea"),this.h()},l(t){e=E(t,"DIV",{class:!0});var i=b(e);n=E(i,"TEXTAREA",{placeholder:!0,oninput:!0,class:!0}),b(n).forEach(g),i.forEach(g),this.h()},h(){s(n,"placeholder","Type here"),s(n,"oninput",'this.style.height = "";this.style.height = this.scrollHeight + "px"'),s(n,"class","svelte-1uhola1"),s(e,"class","idea svelte-1uhola1")},m(t,i){H(t,e,i),_(e,n)},p:y,i:y,o:y,d(t){t&&g(e)}}}class fe extends V{constructor(e){super(),I(this,e,null,ce,L,{})}}function ue(l){let e,n;return{c(){e=N("svg"),n=N("path"),this.h()},l(t){e=M(t,"svg",{width:!0,height:!0,viewBox:!0});var i=b(e);n=M(i,"path",{d:!0,fill:!0}),b(n).forEach(g),i.forEach(g),this.h()},h(){s(n,"d","M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"),s(n,"fill",l[2]),s(e,"width",l[0]),s(e,"height",l[1]),s(e,"viewBox",l[3])},m(t,i){H(t,e,i),_(e,n)},p(t,[i]){i&4&&s(n,"fill",t[2]),i&1&&s(e,"width",t[0]),i&2&&s(e,"height",t[1]),i&8&&s(e,"viewBox",t[3])},i:y,o:y,d(t){t&&g(e)}}}function ge(l,e,n){let{size:t="1em"}=e,{width:i=t}=e,{height:a=t}=e,{color:h="currentColor"}=e,{viewBox:f="0 0 24 24"}=e;return l.$$set=r=>{"size"in r&&n(4,t=r.size),"width"in r&&n(0,i=r.width),"height"in r&&n(1,a=r.height),"color"in r&&n(2,h=r.color),"viewBox"in r&&n(3,f=r.viewBox)},[i,a,h,f,t]}class de extends V{constructor(e){super(),I(this,e,ge,ue,L,{size:4,width:0,height:1,color:2,viewBox:3})}}function me(l){let e,n;return{c(){e=N("svg"),n=N("path"),this.h()},l(t){e=M(t,"svg",{width:!0,height:!0,viewBox:!0});var i=b(e);n=M(i,"path",{d:!0,fill:!0}),b(n).forEach(g),i.forEach(g),this.h()},h(){s(n,"d","M23,10C23,8.89 22.1,8 21,8H14.68L15.64,3.43C15.66,3.33 15.67,3.22 15.67,3.11C15.67,2.7 15.5,2.32 15.23,2.05L14.17,1L7.59,7.58C7.22,7.95 7,8.45 7,9V19A2,2 0 0,0 9,21H18C18.83,21 19.54,20.5 19.84,19.78L22.86,12.73C22.95,12.5 23,12.26 23,12V10M1,21H5V9H1V21Z"),s(n,"fill",l[2]),s(e,"width",l[0]),s(e,"height",l[1]),s(e,"viewBox",l[3])},m(t,i){H(t,e,i),_(e,n)},p(t,[i]){i&4&&s(n,"fill",t[2]),i&1&&s(e,"width",t[0]),i&2&&s(e,"height",t[1]),i&8&&s(e,"viewBox",t[3])},i:y,o:y,d(t){t&&g(e)}}}function _e(l,e,n){let{size:t="1em"}=e,{width:i=t}=e,{height:a=t}=e,{color:h="currentColor"}=e,{viewBox:f="0 0 24 24"}=e;return l.$$set=r=>{"size"in r&&n(4,t=r.size),"width"in r&&n(0,i=r.width),"height"in r&&n(1,a=r.height),"color"in r&&n(2,h=r.color),"viewBox"in r&&n(3,f=r.viewBox)},[i,a,h,f,t]}class ve extends V{constructor(e){super(),I(this,e,_e,me,L,{size:4,width:0,height:1,color:2,viewBox:3})}}function we(l){let e,n;return{c(){e=N("svg"),n=N("path"),this.h()},l(t){e=M(t,"svg",{width:!0,height:!0,viewBox:!0});var i=b(e);n=M(i,"path",{d:!0,fill:!0}),b(n).forEach(g),i.forEach(g),this.h()},h(){s(n,"d","M19,15H23V3H19M15,3H6C5.17,3 4.46,3.5 4.16,4.22L1.14,11.27C1.05,11.5 1,11.74 1,12V14A2,2 0 0,0 3,16H9.31L8.36,20.57C8.34,20.67 8.33,20.77 8.33,20.88C8.33,21.3 8.5,21.67 8.77,21.94L9.83,23L16.41,16.41C16.78,16.05 17,15.55 17,15V5C17,3.89 16.1,3 15,3Z"),s(n,"fill",l[2]),s(e,"width",l[0]),s(e,"height",l[1]),s(e,"viewBox",l[3])},m(t,i){H(t,e,i),_(e,n)},p(t,[i]){i&4&&s(n,"fill",t[2]),i&1&&s(e,"width",t[0]),i&2&&s(e,"height",t[1]),i&8&&s(e,"viewBox",t[3])},i:y,o:y,d(t){t&&g(e)}}}function be(l,e,n){let{size:t="1em"}=e,{width:i=t}=e,{height:a=t}=e,{color:h="currentColor"}=e,{viewBox:f="0 0 24 24"}=e;return l.$$set=r=>{"size"in r&&n(4,t=r.size),"width"in r&&n(0,i=r.width),"height"in r&&n(1,a=r.height),"color"in r&&n(2,h=r.color),"viewBox"in r&&n(3,f=r.viewBox)},[i,a,h,f,t]}class pe extends V{constructor(e){super(),I(this,e,be,we,L,{size:4,width:0,height:1,color:2,viewBox:3})}}function Ee(l){let e,n,t,i,a,h,f,r,o,v,c,d,u,m,B,O;return f=new ve({props:{color:"rgba(0 100 0 / 0.4)",size:"1.4rem"}}),v=new pe({props:{color:"rgba(100 0 0 / 0.4)",size:"1.4rem"}}),u=new de({props:{color:"red",size:"1.4rem"}}),{c(){e=p("div"),n=p("textarea"),t=z(),i=p("div"),a=p("div"),h=p("button"),A(f.$$.fragment),r=z(),o=p("button"),A(v.$$.fragment),c=z(),d=p("button"),A(u.$$.fragment),this.h()},l(w){e=E(w,"DIV",{class:!0,style:!0});var k=b(e);n=E(k,"TEXTAREA",{placeholder:!0,oninput:!0}),b(n).forEach(g),t=C(k),i=E(k,"DIV",{class:!0});var G=b(i);a=E(G,"DIV",{});var P=b(a);h=E(P,"BUTTON",{class:!0});var X=b(h);q(f.$$.fragment,X),X.forEach(g),r=C(P),o=E(P,"BUTTON",{class:!0});var Z=b(o);q(v.$$.fragment,Z),Z.forEach(g),P.forEach(g),c=C(G),d=E(G,"BUTTON",{class:!0});var J=b(d);q(u.$$.fragment,J),J.forEach(g),G.forEach(g),k.forEach(g),this.h()},h(){s(n,"placeholder","Type here"),s(n,"oninput",'this.style.height = "";this.style.height = this.scrollHeight + "px"'),s(h,"class","btn-anim svelte-jqg87s"),s(o,"class","btn-anim svelte-jqg87s"),s(d,"class","btn-anim svelte-jqg87s"),s(i,"class","bottomRow svelte-jqg87s"),s(e,"class","item svelte-jqg87s"),K(e,"--bgColor",l[1])},m(w,k){H(w,e,k),_(e,n),Q(n,l[0]),_(e,t),_(e,i),_(i,a),_(a,h),j(f,h,null),_(a,r),_(a,o),j(v,o,null),_(i,c),_(i,d),j(u,d,null),m=!0,B||(O=[R(n,"input",l[4]),R(h,"click",l[5]),R(o,"click",l[6]),R(d,"click",l[7])],B=!0)},p(w,[k]){k&1&&Q(n,w[0]),(!m||k&2)&&K(e,"--bgColor",w[1])},i(w){m||(x(f.$$.fragment,w),x(v.$$.fragment,w),x(u.$$.fragment,w),m=!0)},o(w){T(f.$$.fragment,w),T(v.$$.fragment,w),T(u.$$.fragment,w),m=!1},d(w){w&&g(e),D(f),D(v),D(u),B=!1,ne(O)}}}function ye(l,e,n){let{info:t="default info"}=e,{id:i=0}=e;const a=ie();let{color:h="lightgrey"}=e;function f(){t=this.value,n(0,t)}const r=()=>{n(1,h=h=="lightgreen"?"lightgrey":"lightgreen")},o=()=>{n(1,h=h=="pink"?"lightgrey":"pink")},v=()=>{a("delete",i)};return l.$$set=c=>{"info"in c&&n(0,t=c.info),"id"in c&&n(2,i=c.id),"color"in c&&n(1,h=c.color)},[t,h,i,a,f,r,o,v]}class ke extends V{constructor(e){super(),I(this,e,ye,Ee,L,{info:0,id:2,color:1})}}function $(l,e,n){const t=l.slice();return t[5]=e[n],t[6]=e,t[7]=n,t}function ee(l){let e,n,t,i;function a(r){l[3](r,l[5])}function h(r){l[4](r,l[5])}let f={id:l[7]};return l[5].info!==void 0&&(f.info=l[5].info),l[5].color!==void 0&&(f.color=l[5].color),e=new ke({props:f}),Y.push(()=>F(e,"info",a)),Y.push(()=>F(e,"color",h)),e.$on("delete",l[2]),{c(){A(e.$$.fragment)},l(r){q(e.$$.fragment,r)},m(r,o){j(e,r,o),i=!0},p(r,o){l=r;const v={};!n&&o&1&&(n=!0,v.info=l[5].info,W(()=>n=!1)),!t&&o&1&&(t=!0,v.color=l[5].color,W(()=>t=!1)),e.$set(v)},i(r){i||(x(e.$$.fragment,r),i=!0)},o(r){T(e.$$.fragment,r),i=!1},d(r){D(e,r)}}}function xe(l){let e,n,t,i,a,h,f,r=l[0],o=[];for(let c=0;c<r.length;c+=1)o[c]=ee($(l,r,c));const v=c=>T(o[c],1,1,()=>{o[c]=null});return{c(){e=p("section");for(let c=0;c<o.length;c+=1)o[c].c();n=z(),t=p("button"),i=U("+"),this.h()},l(c){e=E(c,"SECTION",{class:!0});var d=b(e);for(let m=0;m<o.length;m+=1)o[m].l(d);n=C(d),t=E(d,"BUTTON",{class:!0});var u=b(t);i=S(u,"+"),u.forEach(g),d.forEach(g),this.h()},h(){s(t,"class","addBtn svelte-8jxwzu"),s(e,"class","svelte-8jxwzu")},m(c,d){H(c,e,d);for(let u=0;u<o.length;u+=1)o[u].m(e,null);_(e,n),_(e,t),_(t,i),a=!0,h||(f=R(t,"click",l[1]),h=!0)},p(c,[d]){if(d&5){r=c[0];let u;for(u=0;u<r.length;u+=1){const m=$(c,r,u);o[u]?(o[u].p(m,d),x(o[u],1)):(o[u]=ee(m),o[u].c(),x(o[u],1),o[u].m(e,n))}for(re(),u=r.length;u<o.length;u+=1)v(u);le()}},i(c){if(!a){for(let d=0;d<r.length;d+=1)x(o[d]);a=!0}},o(c){o=o.filter(Boolean);for(let d=0;d<o.length;d+=1)T(o[d]);a=!1},d(c){c&&g(e),oe(o,c),h=!1,f()}}}function Be(l,e,n){let t=[];function i(){console.log("add item"),n(0,t=[...t,{info:"",color:"lightgrey"}])}function a(r){t.splice(r.detail,1),n(0,t)}function h(r,o){l.$$.not_equal(o.info,r)&&(o.info=r,n(0,t))}function f(r,o){l.$$.not_equal(o.color,r)&&(o.color=r,n(0,t))}return[t,i,a,h,f]}class ze extends V{constructor(e){super(),I(this,e,Be,xe,L,{})}}function Ce(l){let e,n,t,i,a,h,f,r,o,v,c,d,u;return r=new he({}),v=new fe({}),d=new ze({}),{c(){e=p("style"),n=U(`@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700;900&display=swap');
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body * {
+            font-family: 'Poppins', sans-serif;
+        } 
+
+        .ctn {
+            max-width: 1280px;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 0.5rem;
+        }
+
+        .btn-anim {
+            transition: 200ms ease-in-out;
+        }
+
+        .btn-anim:hover {
+            transform: scale(1.1);
+        }
+
+        .btn-anim:active {
+            transform: scale(0.9);
+        }
+
+        input {
+            background-color: transparent;
+            border: none;
+            outline: none;
+            width: 100%;
+            word-wrap: break-word;
+            overflow-wrap: break-word;
+            word-break: break-all;
+        }
+
+        input:active,input:focus {
+            border: none;
+            outline: none;
+        }
+
+        textarea {
+            width: 100%;
+            background-color: transparent;
+            border:none;
+            outline: none;
+            resize: none;
+            height: fit-content;
+        }
+
+        textarea:active,textarea:focus {
+            border:none;
+            outline: none;
+        }`),t=z(),i=p("main"),a=p("h1"),h=U(te),f=z(),A(r.$$.fragment),o=z(),A(v.$$.fragment),c=z(),A(d.$$.fragment),this.h()},l(m){const B=se('[data-svelte="svelte-cglm17"]',document.head);e=E(B,"STYLE",{});var O=b(e);n=S(O,`@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700;900&display=swap');
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body * {
+            font-family: 'Poppins', sans-serif;
+        } 
+
+        .ctn {
+            max-width: 1280px;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 0.5rem;
+        }
+
+        .btn-anim {
+            transition: 200ms ease-in-out;
+        }
+
+        .btn-anim:hover {
+            transform: scale(1.1);
+        }
+
+        .btn-anim:active {
+            transform: scale(0.9);
+        }
+
+        input {
+            background-color: transparent;
+            border: none;
+            outline: none;
+            width: 100%;
+            word-wrap: break-word;
+            overflow-wrap: break-word;
+            word-break: break-all;
+        }
+
+        input:active,input:focus {
+            border: none;
+            outline: none;
+        }
+
+        textarea {
+            width: 100%;
+            background-color: transparent;
+            border:none;
+            outline: none;
+            resize: none;
+            height: fit-content;
+        }
+
+        textarea:active,textarea:focus {
+            border:none;
+            outline: none;
+        }`),O.forEach(g),B.forEach(g),t=C(m),i=E(m,"MAIN",{class:!0});var w=b(i);a=E(w,"H1",{contenteditable:!0,class:!0});var k=b(a);h=S(k,te),k.forEach(g),f=C(w),q(r.$$.fragment,w),o=C(w),q(v.$$.fragment,w),c=C(w),q(d.$$.fragment,w),w.forEach(g),this.h()},h(){s(a,"contenteditable","true"),s(a,"class","svelte-1l26zwu"),s(i,"class","ctn")},m(m,B){_(document.head,e),_(e,n),H(m,t,B),H(m,i,B),_(i,a),_(a,h),_(i,f),j(r,i,null),_(i,o),j(v,i,null),_(i,c),j(d,i,null),u=!0},p:y,i(m){u||(x(r.$$.fragment,m),x(v.$$.fragment,m),x(d.$$.fragment,m),u=!0)},o(m){T(r.$$.fragment,m),T(v.$$.fragment,m),T(d.$$.fragment,m),u=!1},d(m){g(e),m&&g(t),m&&g(i),D(r),D(v),D(d)}}}let te="Project Title";class He extends V{constructor(e){super(),I(this,e,null,Ce,L,{})}}export{He as default};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,65 @@
 <svelte:head>
-    <link rel="stylesheet" href="/global.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700;900&display=swap');
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body * {
+            font-family: 'Poppins', sans-serif;
+        } 
+
+        .ctn {
+            max-width: 1280px;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 0.5rem;
+        }
+
+        .btn-anim {
+            transition: 200ms ease-in-out;
+        }
+
+        .btn-anim:hover {
+            transform: scale(1.1);
+        }
+
+        .btn-anim:active {
+            transform: scale(0.9);
+        }
+
+        input {
+            background-color: transparent;
+            border: none;
+            outline: none;
+            width: 100%;
+            word-wrap: break-word;
+            overflow-wrap: break-word;
+            word-break: break-all;
+        }
+
+        input:active,input:focus {
+            border: none;
+            outline: none;
+        }
+
+        textarea {
+            width: 100%;
+            background-color: transparent;
+            border:none;
+            outline: none;
+            resize: none;
+            height: fit-content;
+        }
+
+        textarea:active,textarea:focus {
+            border:none;
+            outline: none;
+        }
+    </style>
 </svelte:head>
 
 <script>
